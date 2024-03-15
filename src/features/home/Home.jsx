23dfda7ce45a8ef264cbda513fd533a6c9026944
roddelapan <j2001.dlrr@gmail.com>
2024-03-14 22:09:19 -0700

    el ancho del home ahora se adapta a las pantallas

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -14,17 +14,23 @@ export const Home = () => {
   const [qt_active, setQtActive] = useState(false)
 
   return (
-    <div className="flex flex-col w-screen h-screen">
+    <div className="
+      flex flex-col 
+      w-screen h-screen
+      bg-light-gray-0 dark:bg-quick7
+      overflow-scroll
+    ">
       <NavBar />
       <div className="
-        grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4  
-        grow overflow-scroll 
-        bg-light-gray-0 dark:bg-quick7    
-        p-4 sm:p-5 md:p-5 lg:p-5
+        w-full md:w-[750px] lg:w-[900px] xl:w-[1200px]
+        mx-auto
+        grid grid-cols-1 gap-2 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4  
       ">
         <Speed_Dial clickHandler={() => { setQtActive(true) }} />
         <Profile name={user_name} avatar={user_avatar} />
-        <div className="text-xl rounded-lg col-span-1 sm:col-span-3 lg:col-span-2 grid gap-3">
+        <div className="
+          grid
+          text-xl rounded-lg col-span-1 sm:col-span-3 lg:col-span-3 xl:col-span-2 gap-2">
           <div className="hidden md:block">
             {
 
@@ -37,7 +43,7 @@ export const Home = () => {
             <Feed posts={posts} setPosts={setPosts} />
           </div>
         </div>
-        <span className="hidden lg:flex row-span-3">
+        <span className="hidden xl:flex row-span-3">
           <Suggestions />
         </span>
       </div>
--- src/features/profile/Profile.jsx
+++ src/features/profile/Profile.jsx
@@ -12,21 +12,32 @@ export const Profile = ({ name, avatar }) => {
 			{/* Profile */}
 			<div className="
         hidden md:block sticky
+				overflow-hidden
 				top-0
 				w-full h-fit
-				dark:bg-quick4 dark:outline dark:outline-1 dark:outline-quick5 text-center text-3xl py-2 rounded-lg row-span-3  lg:bg-white lg:dark:bg-quick4 lg:dark:outline lg:dark:outline-1 lg:dark:outline-quick5 lg:text-center lg:text-3xl lg:py-2 lg:rounded-lg lg:row-span-3  md:bg-white md:dark:bg-quick4 md:dark:outline md:dark:outline-1 md:dark:outline-quick5 md:text-center md:text-3xl md:py-2 md:rounded-lg md:row-span-3">
+				dark:bg-quick4 dark:outline dark:outline-1 
+				dark:outline-quick5 text-center text-3xl 
+				rounded-lg row-span-3  
+				lg:bg-white lg:dark:bg-quick4 
+				lg:dark:outline lg:dark:outline-1 lg:dark:outline-quick5 lg:text-center 
+				lg:text-3xl lg:rounded-lg lg:row-span-3
+				md:bg-white md:dark:bg-quick4 md:dark:outline md:dark:outline-1 
+				md:dark:outline-quick5 md:text-center md:text-3xl md:rounded-lg md:row-span-3">
 				<div className="p-2 flex flex-col border-b dark:border-quick5 items-center">
 					<img className="rounded-full cursor-pointer w-9/12 lg:w-1/2 aspect-square" src={avatar} onClick={() => { navigate("/profile?user=" + user.uid) }} />
 					<h1 className="flex text-xl font-sembold text-black dark:text-white hover:underline hover:cursor-pointer mt-5" onClick={() => { navigate("/profile?user=" + user.uid) }}>
 						{name} {user.subscriptions.indexOf('quicker') !== -1 && <span className='w-4 ml-1'><img src='quicker_badge.png' /></span>}
 					</h1>
 				</div>
-				<div className="antialiased flex flex-col hover:cursor-pointer">
-					<a className="hover:bg-gray-300 dark:hover:bg-quick5 dark:text-white dark:border-quick5 border-b p-3 w-full text-xl text-left text-gray-600 font-semibold" href={"/dms?to=" + user.uid}><i class="fa fa-comment text-gray-600 text-2xl pr-1 pt-1 float-right"></i>Messages</a>
-					<a className="hover:bg-gray-300 dark:hover:bg-quick5 dark:text-white dark:border-quick5 border-b p-3 w-full text-xl text-left text-gray-600 font-semibold" href="/settings"><i class="fa fa-cog text-gray-600 text-2xl pr-1 pt-1 float-right"></i>Settings</a>
+				<div className="
+					flex flex-col
+					divide-y divide-light-grey-border
+				">
+					<a className="hover:bg-gray-300 dark:hover:bg-quick5 dark:text-white` p-3 w-full text-xl text-left text-gray-600 font-semibold" href={"/dms?to=" + user.uid}><i class="fa fa-comment text-gray-600 text-2xl pr-1 pt-1 float-right"></i>Messages</a>
+					<a className="hover:bg-gray-300 dark:hover:bg-quick5 dark:text-white` p-3 w-full text-xl text-left text-gray-600 font-semibold" href="/settings"><i class="fa fa-cog text-gray-600 text-2xl pr-1 pt-1 float-right"></i>Settings</a>
 					{
 						user.role.indexOf('administrator') !== -1 &&
-						<a className="hover:bg-gray-300 dark:hover:bg-quick5 dark:text-white dark:border-quick5 border-b p-3 w-full text-xl text-left text-gray-600 font-semibold" href="/admintools"><i class="fa fa-cog text-gray-600 text-2xl pr-1 pt-1 float-right"></i>Administrator tools</a>
+						<a className="hover:bg-gray-300 dark:hover:bg-quick5 dark:text-white` p-3 w-full text-xl text-left text-gray-600 font-semibold" href="/admintools"><i class="fa fa-cog text-gray-600 text-2xl pr-1 pt-1 float-right"></i>Administrator tools</a>
 					}
 				</div>
 			</div>
