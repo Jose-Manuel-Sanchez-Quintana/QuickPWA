19adee260a41835ae90472cf47f7d0c742d27618
JesusDlrr <j2001.dlrr@gmail.com>
2024-04-17 12:13:42 -0700

    corregidos colores del modal en dark mode

--- src/features/modal/Modal.jsx
+++ src/features/modal/Modal.jsx
@@ -8,7 +8,7 @@ export const Modal = ({ title, message, callback }) => {
     <>
       {isopen && (
         <div className="fixed w-screen h-screen p-5 md:p-0 z-50 inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center ">
-          <div className="bg-white p-4 rounded-md max-w-2xl">
+          <div className="bg-white dark:text-white dark:bg-quick4 p-4 rounded-md max-w-2xl">
             <div className="border-light-grey-border dark:border-light-gray-8 border-b pb-2">
               <p className="font-bold text-center">{title}</p>
             </div>
@@ -21,7 +21,7 @@ export const Modal = ({ title, message, callback }) => {
                   // setIsopen(false);
                   callback(true);
                 }}
-                className="border border-light-grey-border dark:border-light-gray-8 rounded-sm p-1 min-w-16"
+                className="border border-light-grey-border dark:border-light-gray-4 rounded-sm p-1 min-w-16"
               >
                 Yes
               </button>
@@ -30,7 +30,7 @@ export const Modal = ({ title, message, callback }) => {
                   callback(false);
                   // setIsopen(false);
                 }}
-                className="border border-light-grey-border dark:border-light-gray-8rounded-sm p-1 min-w-16"
+                className="border border-light-grey-border dark:border-light-gray-4 rounded-sm p-1 min-w-16"
               >
                 No
               </button>
